5eccabcfc05f55f7fbbfe7441a7a7c9b624d2d3f
Taislin <cariosecus@gmail.com>
2021-02-26 16:07:59 +0000

    removes js files

--- code/preload.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const preload_list = ["icons/error.png"];
-module.exports = async function preload(client) {
-    for (const path of preload_list) {
-        if (!client.icon_metas[path]) {
-            await client.enqueue_icon_meta_load(client, path);
-        }
-    }
-};
